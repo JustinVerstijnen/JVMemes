0c04b7dc8adcd39f4fbcc916d8fc2b7daf588715
Justin Verstijnen <info@justinverstijnen.nl>
2025-11-09 22:14:50 +0100

    Add files via upload

--- concertcounter/script.js
+++ concertcounter/script.js
@@ -1,15 +1,74 @@
-async function loadConcerts() {
-  try {
-    const response = await fetch('concerts.json');
-    if (!response.ok) throw new Error('concerts.json niet gevonden');
-    const concerts = await response.json();
-    renderConcerts(concerts);
-  } catch (err) {
-    console.error('Fout bij laden concerten:', err);
-    document.getElementById("upcoming-list").innerHTML =
-      "<p style='color:red;'>Concertgegevens konden niet worden geladen.</p>";
-  }
-}
+// Concertgegevens – voeg hier nieuwe concerten toe
+const concerts = [
+{ artist: "Guns N' Roses", location: "Goffertpark – Nijmegen", datetime: "2017-07-12T19:30:00" },
+{ artist: "Toto", location: "Ziggo Dome – Amsterdam", datetime: "2018-03-17T20:00:00" }, 
+{ artist: "Guus Meeuwis", location: "Philips Stadion – Eindhoven", datetime: "2018-06-09T20:30:00" }, 
+{ artist: "Guns N' Roses", location: "Goffertpark – Nijmegen", datetime: "2018-07-04T19:30:00" },  
+{ artist: "Europe", location: "013 – Tilburg", datetime: "2018-09-26T21:00:00" },
+{ artist: "Slash & Myles Kennedy", location: "AFAS Live – Amsterdam", datetime: "2019-02-24T20:30:00" },
+{ artist: "The Dire Straits Experience", location: "Kampen Music Club – Kampen", datetime: "2019-05-18T21:00:00" },
+{ artist: "Metallica", location: "Johan Cruijff Arena – Amsterdam", datetime: "2019-06-11T20:45:00" },
+{ artist: "Bon Jovi", location: "Goffertpark – Nijmegen", datetime: "2019-06-13T20:30:00" },
+{ artist: "Guus Meeuwis", location: "Philips Stadion – Eindhoven", datetime: "2019-06-15T20:30:00" },
+{ artist: "Rammstein", location: "De Kuip – Rotterdam", datetime: "2019-06-25T20:45:00" },
+{ artist: "Live", location: "Paradiso – Amsterdam", datetime: "2019-07-02T20:30:00" },
+{ artist: "Alter Bridge", location: "AFAS Live – Amsterdam", datetime: "2019-12-10T20:00:00" },
+{ artist: "Steel Panther", location: "Tivoli Vredenburg – Utrecht", datetime: "2019-02-02T20:00:00" },
+{ artist: "A-ha", location: "AFAS Live – Amsterdam", datetime: "2022-05-04T21:00:00" },
+{ artist: "Disturbed", location: "Ziggo Dome – Amsterdam", datetime: "2025-10-14T20:00:00" },
+{ artist: "Red Hot Chili Peppers", location: "Goffertpark - Nijmegen", datetime: "2022-06-10T20:30:00" },
+{ artist: "Europe", location: "Hello Festival - Emmen", datetime: "2022-06-11T22:45:00" },
+{ artist: "Metallica", location: "Pinkpop Festival - Landgraaf", datetime: "2022-06-17T22:15:00" },
+{ artist: "Guns N' Roses", location: "Stadspark - Groningen", datetime: "2022-06-23T19:45:00" },
+{ artist: "Iron Maiden", location: "Gelredome - Arnhem", datetime: "2022-06-27T21:00:00" },
+{ artist: "Rammstein", location: "Goffertpark - Nijmegen", datetime: "2022-07-04T20:30:00" },
+{ artist: "Rammstein", location: "Goffertpark - Nijmegen", datetime: "2022-07-05T20:30:00" },
+{ artist: "The Killers", location: "Ziggo Dome - Amsterdam", datetime: "2022-07-16T21:00:00" },
+{ artist: "KISS", location: "Ziggo Dome - Amsterdam", datetime: "2022-07-21T21:00:00" },
+{ artist: "Bryan Adams", location: "Ziggo Dome - Amsterdam", datetime: "2022-11-23T20:30:00" },
+{ artist: "Within Temptation", location: "Ziggo Dome - Amsterdam", datetime: "2022-11-30T20:15:00" },
+{ artist: "Evanescence", location: "Ziggo Dome - Amsterdam", datetime: "2022-11-30T21:30:00" },
+{ artist: "Volbeat", location: "Gelredome - Arnhem", datetime: "2022-11-30T21:00:00" },
+{ artist: "Roger Waters", location: "Ziggo Dome - Amsterdam", datetime: "2023-04-04T20:15:00" },
+{ artist: "Avril Lavigne", location: "AFAS Live - Amsterdam", datetime: "2023-04-14T21:00:00" },
+{ artist: "Metallica", location: "Johan Cruijff Arena - Amsterdam", datetime: "2023-04-29T20:30:00" },
+{ artist: "Sabaton", location: "Ziggo Dome - Amsterdam", datetime: "2023-05-03T21:15:00" },
+{ artist: "Normaal", location: "Tractorpulling terrein - Lochem", datetime: "2023-05-18T19:45:00" },
+{ artist: "The Offspring", location: "AFAS Live - Amsterdam", datetime: "2023-05-20T21:15:00" },
+{ artist: "Def Leppard", location: "SparkassenPark - Mönchengladbach", datetime: "2023-05-25T18:45:00" },
+{ artist: "Motley Crue", location: "SparkassenPark - Mönchengladbach", datetime: "2023-05-25T20:45:00" },
+{ artist: "Ghost", location: "AFAS Live - Amsterdam", datetime: "2023-06-04T21:30:00" },
+{ artist: "KISS", location: "Ziggo Dome - Amsterdam", datetime: "2023-06-12T21:00:00" },
+{ artist: "Rammstein", location: "Stadspark - Groningen", datetime: "2023-07-06T20:30:00" },
+{ artist: "Guns N' Roses", location: "Bospop terrein - Weert", datetime: "2023-07-11T19:45:00" },
+{ artist: "Slash & Myles Kennedy", location: "Ziggo Dome - Amsterdam", datetime: "2024-04-09T20:30:00" },
+{ artist: "AC-DC", location: "Johan Cruijff Arena - Amsterdam", datetime: "2024-06-05T20:45:00" },
+{ artist: "Judas Priest", location: "AFAS Live - Amsterdam", datetime: "2024-06-10T21:00:00" },
+{ artist: "Scorpions", location: "Ziggo Dome - Amsterdam", datetime: "2024-06-11T21:00:00" },
+{ artist: "Eagles", location: "Gelredome - Arnhem", datetime: "2024-06-13T21:00:00" },
+{ artist: "Guus Meeuwis", location: "Philips Stadion - Eindhoven", datetime: "2024-06-16T21:00:00" },
+{ artist: "Rammstein", location: "Goffertpark - Nijmegen", datetime: "2024-06-18T20:30:00" },
+{ artist: "Green Day", location: "Gelredome - Arnhem", datetime: "2024-06-19T20:30:00" },
+{ artist: "Steel Panther", location: "Oosterpoort - Groningen", datetime: "2024-06-23T21:00:00" },
+{ artist: "Avenged Sevenfold", location: "Ziggo Dome - Amsterdam", datetime: "2024-06-24T21:00:00" },
+{ artist: "Bruce Springsteen", location: "Goffertpark - Nijmegen", datetime: "2024-06-27T20:00:00" },
+{ artist: "Deep Purple", location: "Ziggo Dome - Amsterdam", datetime: "2024-10-29T21:00:00" },
+{ artist: "Within Temptation", location: "Ziggo Dome - Amsterdam", datetime: "2024-12-06T21:15:00" },
+{ artist: "Toto", location: "Gelredome - Arnhem", datetime: "2025-02-08T21:00:00" },
+{ artist: "Ghost", location: "Ziggo Dome - Amsterdam", datetime: "2025-05-08T21:00:00" },
+{ artist: "Normaal", location: "Tractorpulling terrein - Lochem", datetime: "2025-05-29T20:00:00" },
+{ artist: "John Fogerty", location: "Ziggo Dome - Amsterdam", datetime: "2025-06-23T20:30:00" },
+{ artist: "Linkin Park", location: "Gelredome - Arnhem", datetime: "2025-06-26T20:30:00" },
+{ artist: "Green Day", location: "Ziggo Dome - Amsterdam", datetime: "2025-07-02T20:30:00" },
+{ artist: "DragonForce", location: "Tivoli Vredenburg – Utrecht", datetime: "2025-07-06T21:00:00" },
+{ artist: "Iron Maiden", location: "Gelredome - Arnhem", datetime: "2025-07-23T21:00:00" },
+{ artist: "Alice Cooper", location: "AFAS Live - Amsterdam", datetime: "2025-07-28T20:15:00" },
+{ artist: "The Offspring", location: "Ziggo Dome – Amsterdam", datetime: "2025-11-07T19:30:00" },
+{ artist: "Sabaton", location: "Ziggo Dome – Amsterdam", datetime: "2025-12-01T19:00:00" },
+{ artist: "MetallicA", location: "Deutsche Bank Park – Frankfurt", datetime: "2026-05-22T20:00:00" },
+{ artist: "MetallicA", location: "Deutsche Bank Park – Frankfurt", datetime: "2026-05-24T20:00:00" },
+{ artist: "Iron Maiden", location: "Ziggo Dome – Amsterdam", datetime: "2026-06-10T20:00:00" }
+];
 
 function createConcertCard(concert, isPast) {
   const card = document.createElement("div");
@@ -29,8 +88,7 @@ function createConcertCard(concert, isPast) {
     </div>
   `;
 
-  // Fade-in animatie
-  setTimeout(() => card.classList.add("show"), 50);
+  if (isPast) card.classList.add("past");
   return card;
 }
 
@@ -93,7 +151,7 @@ function isSameDay(d1, d2) {
   return d1.getFullYear() === d2.getFullYear() && d1.getMonth() === d2.getMonth() && d1.getDate() === d2.getDate();
 }
 
-function renderConcerts(concerts) {
+function renderConcerts() {
   const now = new Date();
   const upcomingList = document.getElementById("upcoming-list");
   const archiveList = document.getElementById("archive-list");
@@ -109,8 +167,14 @@ function renderConcerts(concerts) {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  loadConcerts();
+  renderConcerts();
 
   const toggle = document.querySelector(".archive-toggle");
   const arrow = document.querySelector(".arrow");
   const archive = document.getElementById("archive-list");
+
+  toggle.addEventListener("click", () => {
+    archive.classList.toggle("expanded");
+    arrow.style.transform = archive.classList.contains("expanded") ? "rotate(180deg)" : "rotate(0deg)";
+  });
+});
